a8153fade06f4a45cb356b394a6ada50e27b320f
Richard Lawson <lawson.a.richard@gmail.com>
2019-08-22 15:24:54 -0700

    updating
    updating movies is functional but stars.map not proper

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React from "react";
+import ReactDOM from "react-dom";
 
-import './index.css';
-import App from './App'
-import { BrowserRouter as Router } from 'react-router-dom';
+import "./index.css";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+
+// styles
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "shards-ui/dist/css/shards.min.css";
 
 ReactDOM.render(
   <Router>
     <App />
   </Router>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
